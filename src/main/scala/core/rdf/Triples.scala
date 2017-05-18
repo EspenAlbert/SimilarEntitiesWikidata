8643ecc31d9377854375091fbad793ed115709c3
Espen Albert <albertespen@gmail.com>
2017-05-18 13:17:20 +0200

    MetaStatementFactory implemented

--- src/main/scala/core/rdf/Triples.scala
+++ src/main/scala/core/rdf/Triples.scala
@@ -7,5 +7,5 @@ object Triples {
   abstract class ResultStatement(val subject : String, val property: String, val objectValue: String)
   case class OrdinaryStatement(override val subject : String, override val property: String, override val objectValue: String) extends ResultStatement(subject, property, objectValue)
   case class VMStatement(override val subject : String, override val property: String, override val objectValue: String, replacement : (String, String), vmCount: (String, Int)) extends ResultStatement(subject, property, objectValue)
-  case class TMStatement(override val subject : String, override val property: String, override val objectValue: String, entityTypeOrgEntityReplacements : List[(String, String, String)]) extends ResultStatement(subject, property, objectValue)
+  case class TMStatement(override val subject : String, override val property: String, override val objectValue: String, entityEntityTypeOrgEntityReplacements : List[(String, String, String)]) extends ResultStatement(subject, property, objectValue)
 }
